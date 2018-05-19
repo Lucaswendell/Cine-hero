c6fcb624f77f8129da162d04c11471ecc5043fd0
Lucas Wendel <wendellucas303@gmail.com>
2018-05-18 21:14:12 -0300

    Trabalho Js

---
+++ js/meu.js
@@ -0,0 +1,73 @@
+// foundation
+$(document).foundation(); 
+//scroll UP
+$(function () { 
+    $.scrollUp({
+        scrollDistance: 700,
+        animationSpeed: 100,
+        animation: "fade",
+        scrollForm: "top",
+        scrollImg: true,
+        zIndex: 100
+    });
+});
+//fakeloader
+$("#fakeloder").fakeLoader({
+    timeToHide: 2000,
+    spinner: "spinner2",
+    bgColor: "#000",
+});
+//paralax
+$('.parallax-window').parallax(); 
+var slideout = new Slideout({
+    'panel': document.getElementById("panel"),
+    'menu': document.getElementById("menu"),
+    'padding': 256,
+    'tolerance': 70
+});
+$('.toggle-button').on('click', function () {
+    slideout.toggle();
+});
+//trailer
+$("#trailer").click(function () {
+    $.sweetModal({
+        title: "Os vingadores: guerra infinita",
+        content: '<iframe width="100%" height="450" src="https://www.youtube.com/embed/DPOw2hfgCHI?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+        theme: $.sweetModal.THEME_DARK
+    });
+});
+$("#trailer-jovem").click(function () {
+    $.sweetModal({
+        title: "Jovens titãs em ação",
+        content: "<iframe width='100%' height='450' src='https://www.youtube.com/embed/g29xj3Fn26o?rel=0&amp;showinfo=0' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe>",
+        theme: $.sweetModal.THEME_DARK
+    });
+});
+$("#trailer-deadpool").click(function () {
+    $.sweetModal({
+        title: "Deadpool 2",
+        content: "<iframe width='100%' height='450' src='https://www.youtube.com/embed/1P9OzWX6nzE?rel=0&amp;showinfo=0' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe>",
+        theme: $.sweetModal.THEME_DARK
+    });
+});
+$("#trailer-homemForm").click(function () {
+    $.sweetModal({
+        title: "Homem-formiga e a Vespa",
+        content: "<iframe width='100%' height='450' src='https://www.youtube.com/embed/0HGBv7s1Y7E?rel=0&amp;showinfo=0' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe>",
+        theme: $.sweetModal.THEME_DARK
+    });
+});
+$("#trailer-xmen").click(function () { 
+    $.sweetModal({ 
+        title: "X-men: Fênix Negra", 
+        content: "<iframe width='100%' height='450' src='https://www.youtube.com/embed/f4S5xrQBwZA?rel=0&amp;showinfo=0' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe>",
+        theme: $.sweetModal.THEME_DARK
+    }); 
+});
+$("#trailer-venom").click(function(){
+    $.sweetModal({
+        title:"Venom" ,
+        content: "<iframe width='100%' height='450' src='https://www.youtube.com/embed/u9Mv98Gr5pY?rel=0&amp;showinfo=0' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe>",
+        theme: $.sweetModal.THEME_DARK
+    });
+});
